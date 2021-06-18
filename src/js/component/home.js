f2b92d7d1f6e2bb9df0445d28ec55fef944337e7
Naiceiry <naiceirymp@gmail.com>
2021-06-18 17:39:41 +0000

    tres

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,22 +2,24 @@ import React, { useState } from "react";
 export function Home() {
 	const [select, setSelect] = useState("red");
 	return (
-		<div className="semaforo">
-			<div
-				onClick={() => setSelect("red")}
-				className={
-					"select red" + (select === "red" ? "brillo" : "")
-				}></div>
-			<div
-				onClick={() => setSelect("yellow")}
-				className={
-					"select yellow" + (select === "yellow" ? "brillo" : "")
-				}></div>
-			<div
-				onClick={() => setSelect("green")}
-				className={
-					"select green" + (select === "green" ? "brillo" : "")
-				}></div>
+		<div className="text-center mt-5">
+			<div className="semaforo text-center mt-5">
+				<div
+					onClick={() => setSelect("red")}
+					className={
+						"red select " + (select === "red" ? "brillo" : "")
+					}></div>
+				<div
+					onClick={() => setSelect("yellow")}
+					className={
+						"select yellow " + (select === "yellow" ? "brillo" : "")
+					}></div>
+				<div
+					onClick={() => setSelect("green")}
+					className={
+						"select green " + (select === "green" ? "brillo" : "")
+					}></div>
+			</div>
 		</div>
 	);
 }
